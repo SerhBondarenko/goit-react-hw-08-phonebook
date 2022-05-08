03273abf47f50e170a1177590182480454f43637
SerhBondarenko <serhii.bondarenk@gmail.com>
2022-05-08 21:03:06 +0300

    1

---
+++ src/views/ContactsView.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Container from '../components/Container';
+
+import contactsOperations  from '../redux/contacts/contacts-operations';
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactFilter from 'components/ContactFilter/ContactFilter';
+import ContactList from 'components/ContactList/ContactList';
+
+/*
+const barStyles = {
+  display: 'flex',
+  alignItems: 'flex-end',
+  marginBottom: 20,
+};
+*/
+
+export default function ContctsView(params) {
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    function fetchContact(){
+       dispatch(contactsOperations.fetchContacts())
+ }
+ fetchContact()
+ }, [dispatch]);
+
+  return (
+    <Container>
+        <h1>Книга контактів</h1>
+        <ContactForm/>
+        <h2>Контакти</h2>
+        <ContactFilter />
+       <ContactList/>
+      
+
+
+    </Container>
+  );
+}
